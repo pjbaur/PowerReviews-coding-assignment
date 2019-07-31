04f3a3116bb34a7aeeffddcf8dab271df9516d5e
Paul Baur <paul.baur@gmail.com>
2019-07-31 10:07:51 -0500

    remove log files

--- remoteproject/src/test/java/com/powerreviews/project/client/MockRestServiceController.java
+++
@@ -1,7 +0,0 @@
-package com.powerreviews.project.client;
-
-import org.springframework.test.web.client.RequestExpectationManager;
-
-final class MockRestServiceController {
-//	private final RequestExpectationManager expectationManager;
-}
